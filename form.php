052764bae55166ac5a5df23cbc0cbd0ce9320ff6
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-09-30 16:05:12 +0200

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <div>
+        <div>
+            <label for="email">Voer hier je email in</label>
+            <input type="email" name="email" id="email" required>
+            <input type="submit" value="Verstuur">
+        </div>
+    </div>
+
+</body>
+
+</html>
